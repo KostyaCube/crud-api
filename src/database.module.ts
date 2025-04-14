2c3968915ee0e5d0ccf07e402ce82cd517c363a8
KostyaCube <psychocube91@gmail.com>
2025-04-14 17:02:15 +0600

    deps installed, config added

---
+++ src/database.module.ts
@@ -0,0 +1,34 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+type DB_TYPE = 'postgres';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: configService.get<DB_TYPE>('DB_TYPE'),
+        host: configService.get<string>('POSTGRES_HOST'),
+        port: configService.get<number>('POSTGRES_PORT'),
+        username: configService.get<string>('POSTGRES_USER'),
+        password: configService.get<string>('POSTGRES_PASSWORD'),
+        database: configService.get<string>('POSTGRES_DATABASE'),
+        entities:
+          configService.get<string>('NODE_ENV') === 'test'
+            ? ['src/**/*.entity{.js,.ts}']
+            : ['dist/**/*.entity{.ts,.js}'],
+        migrations: ['dist/migrations/**/*{.ts,.js}'],
+        timezone: 'Z',
+        logging: true,
+        migrationsRun: false,
+        autoLoadEntities: true,
+        synchronize: true,
+        cli: { migrationsDir: 'src/migrations' },
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
